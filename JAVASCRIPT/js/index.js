d1dab66aea7dc741f116dd8204913f5459e1c9a0
Daniel Miguel <117693873+DanielMigue@users.noreply.github.com>
2024-04-15 16:41:33 -0300

    02 - Tipos de Saída
    Explorando os tipos de saída do JavaScript

--- JAVASCRIPT/js/index.js
+++ JAVASCRIPT/js/index.js
@@ -1,1 +1,3 @@
-alert('Um alerta rodando de uma página externa');
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!";/*Inserção dado*/
+
+console.log("Oi isso é um console.log");/*Vai na parte do console do navegador ao apertar f12*/
