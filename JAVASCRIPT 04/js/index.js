d42c5e59ecfa27f932c7007197e3e3f31325ad47
Daniel Miguel <117693873+DanielMigue@users.noreply.github.com>
2024-04-16 13:29:50 -0300

    Comentários
    Explorando a maneira de se fazer comentários no JavaScript

--- JAVASCRIPT 04/js/index.js
+++ JAVASCRIPT 04/js/index.js
@@ -1,16 +1,9 @@
-console.log(5+5);/*Ele faz a operação */
-console.log("5+5");/*texto */
-console.log(1);/*é um número */
+//Se faz o comentario através do barra barrra'//'
 
-var a=1;
-var b=5;
-var c=a+b;
-console.log(c);/*Mostra o resultado de a + b */
-
-/*JavaScript é case sensitive*/
-
-/*Nao pode usar hífen 
-tipo "primeiro-nome"
-
-normalmente usa-se o camelCase primeiroNome 
-ou primeiro_nome*/
+/**
+ Tudo
+ que fica dentro
+ fica comentado com
+  multiplas 
+  linhas
+ */
