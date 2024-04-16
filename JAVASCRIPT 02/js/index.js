ff41e376158b7016549d7396dda5d3d37c582001
Daniel Miguel <117693873+DanielMigue@users.noreply.github.com>
2024-04-16 12:56:41 -0300

    TIPOS DE SAÍDA
    Explorando os tipos de saída

---
+++ JAVASCRIPT 02/js/index.js
@@ -0,0 +1,3 @@
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!";/*Inserção dado*/
+
+console.log("Oi isso é um console.log");/*Vai na parte do console do navegador ao apertar f12*/
