9b9e350d9d540e18aaa2378ae44e8eb6c6757af6
Daniel Miguel <117693873+DanielMigue@users.noreply.github.com>
2024-04-16 13:24:56 -0300

    Sintaxe
    Explorando a sintaxe

---
+++ JAVASCRIPT 04/js/index.js
@@ -0,0 +1,16 @@
+console.log(5+5);/*Ele faz a operação */
+console.log("5+5");/*texto */
+console.log(1);/*é um número */
+
+var a=1;
+var b=5;
+var c=a+b;
+console.log(c);/*Mostra o resultado de a + b */
+
+/*JavaScript é case sensitive*/
+
+/*Nao pode usar hífen 
+tipo "primeiro-nome"
+
+normalmente usa-se o camelCase primeiroNome 
+ou primeiro_nome*/
