833064e7171d1eb422e8e2fbb33d927c356ef1b6
Daniel Miguel <117693873+DanielMigue@users.noreply.github.com>
2024-04-17 11:17:08 -0300

    Operadores
    Explorando os operadores do JavaScript

---
+++ JAVASCRIPT 07/js/index.js
@@ -0,0 +1,115 @@
+
+
+// var valor1, valor2, total;
+// valor1=5;
+// valor2=2;
+
+// total=valor1 / valor2;//= operador de atribuição
+// alert(total)
+
+//____________________________________________
+
+
+// var valor1, valor2, total;
+// valor1=5;
+// valor2=2;
+
+// total= ++valor1 //Ele incrementa +1//mesma coisa com --
+// alert(total);//fica 6
+// alert('O valor ficou: '+ valor1);//o valor1 fica 6
+//____________________________________________
+
+// var valor1, valor2, total;
+// valor1 =  5;
+// valor2 = 2;
+
+// valor1 += valor2;//isso é a mesma coisa que a linha 26
+// valor1 = valor1 + valor2;//mesma coisa que  alinha 25
+//_________________________________________
+
+// var valor1, valor2, total;
+// valor1 =  "Dimitri ";
+// valor2 = "Teixeira";
+
+// total = valor1 + valor2;//concatenação de Strings
+// alert(total)
+//_______________________________________
+
+// var valor1, valor2, total;
+// valor1 =  8;
+// valor2 = 8;
+
+//operadores de Comparação
+// total = (valor1 == valor2)//true(verdadeiro) ou false(falso)
+// alert(total)
+
+//______________________________________
+// var valor1, valor2, total;
+// valor1 =  8;
+// valor2 = "8";
+
+// //operadores de Comparação
+// total = (valor1 === valor2)//Ele verifica se o valor é igual e o tipo, como é um int com string é falso
+// alert(total)
+//____________________________
+// var valor1, valor2, total;
+// valor1 =  8;
+// valor2 = 8;
+
+// //operadores de Comparação
+// total = (valor1 != valor2)//Não igual "!="
+// alert(total)
+//_________________________________
+
+// var valor1, valor2, total;
+// valor1 =  8;
+// valor2 = 12;
+
+// //operadores de Comparação
+// total = (valor1 != valor2)//Não igual "!=", pra ver valor e tipo também teria qu fazer assim"!=="
+// alert(total)
+//________________________________
+// var valor1, valor2, total;
+// valor1 =  8;
+// valor2 = 12;
+
+// //operadores de Comparação
+// //true ou false
+// total = (valor1 < 4)//"<"valor1 é menor, ">"maior,"<=" menor e igual,">=" maior e igual.
+// alert(total)
+//_______________________________________________
+// var idade, eleitor;
+// idade=19;
+// eleitor=(idade<18) ? "Não Eleitor":"Sim, eleitor";//Operador codicional(ternário)
+
+// alert('A resposta é: ' + eleitor + ' a idade dele é de: ' + idade);
+//________________________________________________
+// var idade, eleitor;
+// idade=19;
+
+// //operador lógico
+// resultado = (idade > 60 && idade < 70); //true //false
+// alert(resultado)
+
+//_________________________________________________
+// var idade, eleitor;
+// idade=65;
+
+// //operador lógico
+// //ou
+// resultado = (idade===65 || idade===72); //true //false
+// alert(resultado)
+// //_________________________________________________
+// var idade, eleitor;
+// idade=65;
+
+// //operador lógico
+// //ou
+// resultado = !(idade===65); //true //false para o contrario do que ta ali dentro
+// alert(resultado)
+
+
+// && = e, // ||= ou, !=negação
+
+//leitura complementar
+//https://bit.ly/3etfA43
