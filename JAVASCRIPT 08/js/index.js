59b96e06b51a02071250646154c696e41019d62c
Daniel Miguel <117693873+DanielMigue@users.noreply.github.com>
2024-04-18 11:16:30 -0300

    Funções
    Explorando as funções no JavaScript

---
+++ JAVASCRIPT 08/js/index.js
@@ -0,0 +1,46 @@
+
+//FUNCAO DE SOMA
+function soma(valor1,valor2){
+    return valor1 + valor2;
+}
+
+// document.getElementById("texto").innerHTML = soma(10,23);//aqui aparece no html
+
+// var total = soma(10,23)//aqui aparece a notificação do navegador
+
+// alert(total)
+//___________________________________________
+
+//FUNCAO DE COTACAO DO DOLAR
+function realParaDolar(real, cotacaoDolar){
+    return real * cotacaoDolar;
+}
+
+// var valorReal = 7.89;
+// var cotacao = 5.08;
+
+// var total = realParaDolar(valorReal, cotacao);
+
+// alert("O valor em real é R$: " + valorReal + " o valor em dólar U$ é: " +  total)//retorna no valor do dolar
+//___________________________________________
+//FUNCAO DE SAUDACAO
+// function alertaHello(){
+//     alert("Olá pessoal")
+// }
+
+// alertaHello();
+//___________________________________________
+//FUNCAO DE CONVERSAO DE TEMPERATURA
+// function paraCelsius(valorFahrenheit){
+//     return (5/9) * (valorFahrenheit-32);
+// }
+
+// var x = paraCelsius(77);
+
+// alert("A temperatura é de " + x + " graus celsius");
+//___________________________________________
+function minhaFuncao(){//nao utilizar caracteres especiais
+    //variavel local
+    var x =2;//funciona somente no bloco da função, a nao ser que em cima da funcao ela ja esteja declarada
+
+}
